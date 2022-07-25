91e2a0c76b7dd6f9c3b399d97e8d6bf7222d70d2
Ioritz Eguileor <66511137+ioritzeguileor@users.noreply.github.com>
2022-07-24 21:20:22 -0400

    new updates entry

--- pages/updates.js
+++ pages/updates.js
@@ -1,4 +1,4 @@
-import { Container, Heading } from '@chakra-ui/react'
+import { Container, Heading,Link } from '@chakra-ui/react'
 import Layout from '../components/layouts/article'
 import Entry from '../components/journalentry'
 
@@ -11,6 +11,9 @@ const Posts = () => (
       <Entry date="June 30 2022">First portfolio website, started and completed :)</Entry>
       <Entry date="July 15 2022">Began working on React Dashboard Project</Entry>
       <Entry date="July 21 2022">Finishing website up then will continue to review code</Entry>
+      <Entry date="July 24 2022">1/3rd of the way through <Link href="https://www.udemy.com/course/python-for-finance-and-trading-algorithms/">Udemy Python for Financial Analysis </Link>course.
+      Learned how to use pynum as well as pandas and now will get into how to use matplotlib for visual analysis.
+      </Entry>
     </Container>
   </Layout>
 )
