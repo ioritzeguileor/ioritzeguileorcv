edfea130cea6205f20500fb9d3e888ee2a4509b2
Ioritz Eguileor <66511137+ioritzeguileor@users.noreply.github.com>
2022-08-10 16:57:13 -0400

    Update footer.js

--- components/footer.js
+++ components/footer.js
@@ -3,7 +3,7 @@ import { Box } from '@chakra-ui/react'
 const Footer = () => {
   return (
     <Box align="center" opacity={0.4} fontSize="sm">
-      &copy; {new Date().getFullYear()} Ioritz Eguileor & CraftzDog. 
+      &copy; {new Date().getFullYear()} Ioritz Eguileor. 
     </Box>
   )
 }
