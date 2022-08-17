657a5098424277d9e618e4609ab13f77c318c846
Ioritz Eguileor <66511137+ioritzeguileor@users.noreply.github.com>
2022-08-17 09:12:49 -0400

    Update resume.js

--- pages/resume.js
+++ pages/resume.js
@@ -20,7 +20,6 @@ const Resume=()=>{
         <Layout title="Resume">
             <Container>
                 <Heading as="h3" variant="section-title" fontSize={20}>Resume</Heading>
-                    <p>This is a mock resume not my actual one as it is not yet completed.</p>
                 <Box>
                 <Document file="/ioritz_resume_current.docx (3).pdf" onLoadSuccess={onDocumentLoadSuccess}>
                     <Page height="900" pageNumber={pageNumber}/>
